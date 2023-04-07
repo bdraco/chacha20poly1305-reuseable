2b8b2c839c9861d91ec1e94b1f1d1df19ba5dec8
J. Nick Koston <nick@koston.org>
2023-04-07 09:32:21 -1000

    feat: add cython backend

---
+++ build_ext.py
@@ -0,0 +1,35 @@
+"""Build optional cython modules."""
+
+import os
+from distutils.command.build_ext import build_ext
+
+
+class BuildExt(build_ext):
+    def build_extensions(self):  # type: ignore
+        try:
+            super().build_extensions()
+        except Exception:  # nosec
+            pass
+
+
+def build(setup_kwargs):  # type: ignore
+    if os.environ.get("SKIP_CYTHON", False):
+        return
+    try:
+        from Cython.Build import cythonize
+
+        setup_kwargs.update(
+            dict(
+                ext_modules=cythonize(
+                    [
+                        "src/chacha20poly1305_reuseable/__init__.py",
+                    ],
+                    compiler_directives={"language_level": "3"},  # Python 3
+                ),
+                cmdclass=dict(build_ext=BuildExt),
+            )
+        )
+    except Exception:
+        if os.environ.get("REQUIRE_CYTHON"):
+            raise
+        pass
